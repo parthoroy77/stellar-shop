e25185f647266fe2efa01eb23247261547e46bb6
Partho Roy <roypartho772@gmail.com>
2024-08-01 22:47:22 +0600

    ADDED: detail page layout

--- apps/web/app/(HomeLayout)/products/[...id]/page.tsx
+++ apps/web/app/(HomeLayout)/products/[...id]/page.tsx
@@ -1,5 +1,10 @@
-const ProductPage = () => {
-  return <div>hi</div>;
+const ProductDetailPage = () => {
+  return (
+    <div className="flex h-screen py-10">
+      <div className="flex w-[75%] bg-blue-50"></div>
+      <div className="w-[25%] bg-gray-200"></div>
+    </div>
+  );
 };
 
-export default ProductPage;
+export default ProductDetailPage;
