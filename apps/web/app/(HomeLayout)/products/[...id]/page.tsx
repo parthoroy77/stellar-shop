2fee05ed6538849ccb26bb3e2347b8798c10a2be
Partho Roy <roypartho772@gmail.com>
2024-08-02 11:13:30 +0600

    ADDED: detail page web & mob layout

--- apps/web/app/(HomeLayout)/products/[...id]/page.tsx
+++ apps/web/app/(HomeLayout)/products/[...id]/page.tsx
@@ -1,10 +1,50 @@
 const ProductDetailPage = () => {
   return (
-    <div className="flex h-screen py-10">
-      <div className="flex w-[75%] bg-blue-50"></div>
-      <div className="w-[25%] bg-gray-200"></div>
+    <div className="py-5 lg:py-10">
+      {/* Web Layout */}
+      
+      {/* Mobile Layout */}
+      <div className="space-y-5 *:border *:p-4 lg:hidden">
+        <ProductImageSlider />
+        <ProductMainInfo />
+        <PriceRatingBadgeOptions />
+        <PreferenceOptions />
+        <DeliveryPaymentInfo />
+        <div>
+          <button>Buy Now</button>
+        </div>
+        <Reviews />
+        <SellerInfo />
+        <ProductSuggestions />
+        <FAQ />
+        <Specification />
+        <Description />
+      </div>
     </div>
   );
 };
+
+const ProductImageSlider = () => <div>Product Image Slider with magnification effect</div>;
+const ProductMainInfo = () => <div>Product main infos</div>;
+const PriceRatingBadgeOptions = () => <div>Price & Ratings & Badges & Options</div>;
+const DeliveryPaymentInfo = () => <div>Delivery Infos & Payment Infos</div>;
+const PreferenceOptions = () => <div>Preference Options</div>;
+const SellerInfo = () => <div>Seller Info</div>;
+const OptionalInfo = () => <div>Some infos Optional</div>;
+const TabNavigation = () => (
+  <div>
+    <ul className="flex space-x-4 border-b">
+      <li>Specification</li>
+      <li>Description</li>
+      <li>Review</li>
+      <li>FAQ</li>
+    </ul>
+  </div>
+);
+const ProductSuggestions = () => <div>Product Suggestions</div>;
+const Reviews = () => <div>Reviews</div>;
+const FAQ = () => <div>FAQ</div>;
+const Specification = () => <div>Specification</div>;
+const Description = () => <div>Description</div>;
 
 export default ProductDetailPage;
