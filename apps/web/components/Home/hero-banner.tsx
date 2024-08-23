c23f4d2ff905f5cd14491edba95b842d57fd53af
Partho Roy <roypartho772@gmail.com>
2024-08-23 19:47:32 +0600

    ADDED: banner slider

--- apps/web/components/Home/hero-banner.tsx
+++ apps/web/components/Home/hero-banner.tsx
@@ -1,34 +1,61 @@
+"use client";
 import { Button } from "@repo/ui";
+import React from "react";
+import "swiper/css";
+import "swiper/css/navigation";
+import { Autoplay, Navigation } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "../../styles/slider.css";
+
+const banners = [
+  { img: "https://klbtheme.com/bevesi/wp-content/uploads/2024/04/slider-01.jpg" },
+  { img: "https://klbtheme.com/bevesi/wp-content/uploads/2024/04/slider-02.jpg" },
+  { img: "https://klbtheme.com/bevesi/wp-content/uploads/2024/04/slider-03.jpg" },
+  { img: "https://klbtheme.com/bevesi/wp-content/uploads/2024/04/slider-01.jpg" },
+];
 
 const HeroBanner = () => {
   return (
-    <div className="relative h-[550px] w-full overflow-hidden rounded-md lg:w-[58%]">
-      <img
-        className="h-full w-full rounded-md object-cover"
-        src="https://ninetheme.com/themes/fitment/wp-content/uploads/2023/08/blog-post-5.png"
-        alt="Banner Image"
-      />
-      <div className="absolute top-0 h-full w-full bg-gradient-to-r from-gray-900 to-black opacity-40" />
-      <div className="absolute top-0 flex h-full w-full flex-col justify-center gap-6 p-8 text-white">
-        <Button variant={"outline"} className="text-primary w-fit px-8 uppercase tracking-wide" size={"sm"}>
-          From $320
-        </Button>
-        <h1 className="text-3xl font-semibold lg:text-4xl">Trends Car Rims</h1>
-        <p className="text-sm lg:w-[80%] lg:text-lg">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magnam, tempora voluptates dicta incidunt commodi
-          magni maxime nobis debitis nesciunt obcaecati dolorem a
-        </p>
-        <div className="space-x-3">
-          <Button className="px-8 text-sm text-white" size={"sm"}>
-            From $320
-          </Button>
-          <Button className="bg-secondary px-8 text-sm text-white" size={"sm"}>
-            From $320
-          </Button>
-        </div>
-      </div>
+    <div className="h-[550px] w-full overflow-hidden rounded-md lg:w-[58%]">
+      <Swiper
+        loop={true}
+        autoplay={{
+          delay: 3500,
+          disableOnInteraction: false,
+          pauseOnMouseEnter: true,
+        }}
+        navigation
+        modules={[Navigation, Autoplay]}
+        className="h-full w-full"
+      >
+        {banners.map(({ img }, index) => (
+          <SwiperSlide key={index}>
+            <div className="relative h-full w-full">
+              <img
+                className="h-full w-full rounded-md object-fill object-center"
+                src={img}
+                alt={`Banner ${index + 1}`}
+                loading="lazy"
+              />
+              <div className="absolute inset-0 bg-gradient-to-r from-gray-900 to-black opacity-50" />
+              <div className="absolute inset-0 flex flex-col justify-center gap-6 p-8 text-white">
+                <h1 className="text-3xl font-semibold lg:text-4xl">Trends Car Rims</h1>
+                <p className="text-sm lg:w-[80%] lg:text-lg">
+                  Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magnam, tempora voluptates dicta incidunt
+                  commodi magni maxime nobis debitis nesciunt obcaecati dolorem a.
+                </p>
+                <div className="space-x-3">
+                  <Button className="px-8 text-sm text-white" size="sm">
+                    Shop Now!!
+                  </Button>
+                </div>
+              </div>
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
     </div>
   );
 };
 
-export default HeroBanner;
+export default React.memo(HeroBanner);
