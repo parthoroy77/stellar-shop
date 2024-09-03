2824a611db3be7452732c05229c399313c07abf0
Partho Roy <roypartho772@gmail.com>
2024-09-03 20:51:28 +0600

    ADDED: common component for search & category page

--- apps/web/app/(HomeLayout)/(SearchAndCategoryLayout)/categories/[category-slug]/page.tsx
+++ apps/web/app/(HomeLayout)/(SearchAndCategoryLayout)/categories/[category-slug]/page.tsx
@@ -1,5 +1,13 @@
+import ProductListing from "@/components/SearchAndCategoryPage/product-listing";
+import SideFilters from "@/components/SearchAndCategoryPage/side-filters";
+
 const CategoryPage = () => {
-  return <div>Category Wise Product Page</div>;
+  return (
+    <div className="flex gap-5">
+      <SideFilters />
+      <ProductListing />
+    </div>
+  );
 };
 
 export default CategoryPage;
--- apps/web/components/SearchAndCategoryPage/product-listing.tsx
+++ apps/web/components/SearchAndCategoryPage/product-listing.tsx
@@ -1,39 +1,33 @@
-import SideFilters from "@/components/ProductSearchPage/side-filters";
 import ProductCard from "@/components/ui/product-card";
 import { Select, SelectContent, SelectGroup, SelectItem, SelectTrigger, SelectValue } from "@repo/ui";
 import { productsPerViewOptions, sortOptions } from "@repo/utils/constants";
-import { FC } from "react";
-interface ProductSearchPageProps {
-  params: {
-    collections: string[];
-  };
-}
-const ProductSearchPage: FC<ProductSearchPageProps> = ({ params }) => {
+const ProductListing = () => {
   return (
-    <div className="flex gap-5 py-10">
-      <div className="w-[21%]">
-        <SideFilters />
-      </div>
-      <div className="w-[79%] space-y-5">
-        <div className="flex w-full items-center justify-between rounded-md">
-          <span>0 items found</span>
-          <div className="flex gap-5">
+    <div className="w-[80%] space-y-5">
+      <div className="flex w-full items-center justify-between rounded-md">
+        <span className="text-accent-foreground inline-block text-xs">Showing all 7 results</span>
+        <div className="flex gap-5">
+          <div className="flex items-center gap-2">
+            <span className="text-accent-foreground block text-xs">Show:</span>
             <Select>
-              <SelectTrigger className="min-w-[100px]">
+              <SelectTrigger className="w-[100px]">
                 <SelectValue placeholder="Show" />
               </SelectTrigger>
               <SelectContent>
                 <SelectGroup>
                   {productsPerViewOptions.map((op) => (
                     <SelectItem key={op.value} value={op.value.toString()}>
-                      {op.label}
+                      {op.label} Item
                     </SelectItem>
                   ))}
                 </SelectGroup>
               </SelectContent>
             </Select>
+          </div>
+          <div className="flex items-center gap-2">
+            <span className="text-accent-foreground block text-xs">Sort:</span>
             <Select>
-              <SelectTrigger className="min-w-[180px]">
+              <SelectTrigger className="w-[180px]">
                 <SelectValue placeholder="Sort By" />
               </SelectTrigger>
               <SelectContent>
@@ -48,14 +42,14 @@ const ProductSearchPage: FC<ProductSearchPageProps> = ({ params }) => {
             </Select>
           </div>
         </div>
-        <div className="grid grid-cols-4 gap-5">
-          {Array.from({ length: 8 }).map((_x, index) => (
-            <ProductCard key={index} />
-          ))}
-        </div>
+      </div>
+      <div className="grid grid-cols-4 gap-5">
+        {Array.from({ length: 8 }).map((_x, index) => (
+          <ProductCard key={index} />
+        ))}
       </div>
     </div>
   );
 };
 
-export default ProductSearchPage;
+export default ProductListing;
