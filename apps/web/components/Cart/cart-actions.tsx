eaba6849ffa8cefaf227fd98bee4140f971f9577
Partho Roy <roypartho772@gmail.com>
2024-11-19 21:51:50 +0600

    mod: cart action and cart item design

--- apps/web/components/Cart/cart-actions.tsx
+++ apps/web/components/Cart/cart-actions.tsx
@@ -1,19 +1,22 @@
-import { Checkbox } from "@ui/index";
-import { MdDelete } from "react-icons/md";
+import { AppButton } from "@ui/index";
+import { HiXMark } from "react-icons/hi2";
 
 const CartActions = () => {
   return (
     <div className="text-accent-foreground flex items-center justify-between rounded-md border px-6 py-4 text-sm">
       {/* Select All */}
-      <div className="flex items-center gap-2 uppercase">
-        <Checkbox />
-        <span>Select All (0 Items)</span>
+      <div className="flex items-center gap-2 font-medium capitalize text-black">
+        <span>Your Cart (3 Items)</span>
       </div>
       {/* Delete Action */}
-      <button className="flex items-center gap-1 uppercase">
-        <MdDelete size={18} className="text-primary" />
-        <span>Delete</span>
-      </button>
+      <AppButton
+        size={"sm"}
+        variant={"outline"}
+        className="hover:bg-accent hover:text-primary flex h-6 items-center gap-1 border-0 text-xs"
+      >
+        <HiXMark size={18} className="text-primary" />
+        <span>Clear</span>
+      </AppButton>
     </div>
   );
 };
--- apps/web/components/Cart/cart-item.tsx
+++ apps/web/components/Cart/cart-item.tsx
@@ -1,7 +1,8 @@
 import { TProduct } from "@repo/utils/types";
-import { Checkbox } from "@ui/index";
+import { AppButton, Checkbox } from "@ui/index";
 import Image from "next/image";
 import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
+import { RiDeleteBinLine } from "react-icons/ri";
 import { TbCurrencyTaka } from "react-icons/tb";
 
 type CartItemProps = {
@@ -10,7 +11,7 @@ type CartItemProps = {
 
 const CartItem = ({ product }: CartItemProps) => {
   return (
-    <div className="flex items-center gap-3 px-4 py-4 lg:items-center lg:px-6 lg:py-3">
+    <div className="hover:bg-accent/40 flex items-center gap-3 px-4 py-4 lg:items-center lg:px-6 lg:py-3">
       <Checkbox className="size-5 lg:size-4" />
       <div className="flex w-full items-start justify-between gap-3 lg:items-center">
         <Image
@@ -22,7 +23,7 @@ const CartItem = ({ product }: CartItemProps) => {
         />
         <div className="flex w-full flex-col justify-between gap-2 lg:flex-row lg:items-center">
           <h5 className="text-sm font-medium lg:w-[65%]">{product.productName}</h5>
-          <div className="flex flex-row-reverse justify-between gap-5 lg:flex-row lg:items-center">
+          <div className="flex-row- flex justify-between gap-5 lg:flex-row lg:items-center">
             <div className="flex items-center gap-5 rounded-md border p-2 text-xs md:min-w-24">
               <button>
                 <AiOutlineMinus size={14} />
@@ -32,9 +33,14 @@ const CartItem = ({ product }: CartItemProps) => {
                 <AiOutlinePlus size={14} />
               </button>
             </div>
-            <div className="flex items-center justify-start text-sm font-medium text-black md:min-w-24 lg:justify-center">
+            <div className="flex items-center justify-start text-sm font-medium text-black md:min-w-24 lg:justify-center lg:text-lg">
               <TbCurrencyTaka />
               <span>{product.price}</span>
+            </div>
+            <div className="flex items-center">
+              <AppButton variant={"accent"} size={"sm"} className="h-fit w-fit border p-1 lg:p-2">
+                <RiDeleteBinLine className="text-sm lg:text-xl" />
+              </AppButton>
             </div>
           </div>
         </div>
