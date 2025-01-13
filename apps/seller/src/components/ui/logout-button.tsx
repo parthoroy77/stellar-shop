a66156b22d7d46ef63446b5072cd8e981bc1f46f
Partho Roy <roypartho772@gmail.com>
2025-01-13 21:03:03 +0600

    feat: integrated useTransition in logout buttons of web and seller

--- apps/seller/src/components/ui/logout-button.tsx
+++ apps/seller/src/components/ui/logout-button.tsx
@@ -1,18 +1,22 @@
 "use client";
 import { AppButton } from "@ui/index";
 import { signOut } from "next-auth/react";
+import { useTransition } from "react";
 import { LuLogOut } from "react-icons/lu";
 
 const LogoutButton = () => {
+  const [isPending, startTransition] = useTransition();
+
   return (
     <AppButton
       type="button"
       size={"sm"}
       variant={"ghost"}
-      onClick={() => signOut()}
+      loading={isPending}
+      onClick={() => startTransition(() => signOut())}
       className="flex h-fit w-full items-center justify-start gap-2 !px-0 text-sm"
     >
-      <LuLogOut />
+      {!isPending && <LuLogOut />}
       <span>Logout</span>
     </AppButton>
   );
--- apps/web/components/Navbar/nav-logout-button.tsx
+++ apps/web/components/Navbar/nav-logout-button.tsx
@@ -1,16 +1,20 @@
 "use client";
-
+import { AppButton } from "@ui/index";
 import { signOut } from "next-auth/react";
+import { useTransition } from "react";
 import { CiLogout } from "react-icons/ci";
 
 export default function NavLogoutButton() {
+  const [isPending, startTransition] = useTransition();
   return (
-    <button
-      onClick={() => signOut()}
-      className="flex w-full items-center gap-3 rounded-md px-2 py-1 text-sm font-medium text-gray-700 hover:bg-gray-100"
+    <AppButton
+      loading={isPending}
+      variant={"ghost"}
+      onClick={() => startTransition(() => signOut())}
+      className="flex h-fit w-full justify-start gap-3 rounded-md px-2 py-1.5 text-sm font-normal text-gray-700 hover:bg-gray-100"
     >
-      <CiLogout size={18} />
+      {!isPending && <CiLogout size={18} />}
       <span>Logout</span>
-    </button>
+    </AppButton>
   );
 }
