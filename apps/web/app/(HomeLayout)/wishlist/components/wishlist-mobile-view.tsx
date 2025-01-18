3e5a7d1587c2731c266589d52d8b088a9a60e5c7
Partho Roy <roypartho772@gmail.com>
2025-01-18 22:51:05 +0600

    feat(web): enhanched wishlist item card design in mobile view

--- apps/web/app/(HomeLayout)/wishlist/components/wishlist-mobile-view.tsx
+++ apps/web/app/(HomeLayout)/wishlist/components/wishlist-mobile-view.tsx
@@ -14,48 +14,48 @@ const WishlistMobileView = ({ wishlistItems }: { wishlistItems: TWishlistItem[] 
         wishlistItems.map((wishlistItem, idx) => (
           <div key={idx} className="space-y-1.5 px-2">
             <div className="flex items-start gap-3">
-              <Link href={`/products/${wishlistItem.product.urlSlug}`} className="block w-[30%]">
-                <div className="w-full rounded-md border p-1">
-                  <Image
-                    width={70}
-                    height={70}
-                    src={wishlistItem.product.images[0]?.file.fileSecureUrl!}
-                    alt={wishlistItem.product.productName}
-                    className="w-full rounded-md object-cover"
-                  />
-                </div>
+              <Link href={`/products/${wishlistItem.product.urlSlug}`} className="block w-[25%]">
+                <Image
+                  width={70}
+                  height={70}
+                  src={wishlistItem.product.images[0]?.file.fileSecureUrl!}
+                  alt={wishlistItem.product.productName}
+                  className="w-full rounded-md border object-cover p-1"
+                />
               </Link>
-              <div className="flex h-full w-[70%] flex-col justify-between space-y-1">
+              <div className="flex h-full w-[75%] flex-col justify-between space-y-2">
                 <Link href={`/products/${wishlistItem.product.urlSlug}`}>
                   <h5 className="truncate font-medium">{wishlistItem.product.productName}</h5>
                 </Link>
                 {/* If has variant then show here */}
-                {getAttributes.length > 0 && (
-                  <div className="flex flex-wrap gap-2">
-                    {getAttributes(wishlistItem)?.map((attr, idx) => (
-                      <span key={idx} className="bg-accent/40 rounded-md border px-3 py-0.5 text-xs font-medium">
-                        {attr.name}
-                      </span>
-                    ))}
-                  </div>
-                )}
 
-                <div className="flex items-center justify-between gap-2 text-xs">
+                <div className="flex items-center justify-between gap-2 text-sm">
                   <div className="text-primary-foreground flex items-center gap-1 font-medium">
                     <LuDollarSign />
                     <span>90.99</span>
                   </div>
+                  {getAttributes.length > 0 && (
+                    <div className="flex flex-wrap gap-2">
+                      {getAttributes(wishlistItem)?.map((attr, idx) => (
+                        <span key={idx} className="bg-accent/40 rounded-md border px-3 py-0.5 text-xs font-medium">
+                          {attr.name}
+                        </span>
+                      ))}
+                    </div>
+                  )}
+                </div>
+                <div className="flex items-center justify-between gap-2 text-xs">
                   <Badge variant={"success"} className="rounded-md">
                     In stock
                   </Badge>
-                </div>
-                <div className="flex items-center justify-between gap-2 text-xs">
-                  <RemoveWishlistButton
-                    productId={wishlistItem.productId}
-                    isMobile
-                    productVariantId={wishlistItem.productVariantId || undefined}
-                  />
-                  <AddToCartButton productId={wishlistItem.product.id} />
+                  <div className="flex items-center gap-2">
+                    <RemoveWishlistButton
+                      productId={wishlistItem.productId}
+                      isMobile
+                      productVariantId={wishlistItem.productVariantId || undefined}
+                    />
+                    <AddToCartButton productId={wishlistItem.product.id} />
+                  </div>
                 </div>
               </div>
             </div>
