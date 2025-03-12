d2b6da77b6c971f0d3b2cfb14d2eb00c716f6a36
Partho Roy <roypartho772@gmail.com>
2025-03-12 20:00:37 +0600

    feat(admin): add seller list page and shown data into table

---
+++ apps/admin/src/components/data-tables/attributes/column.tsx
@@ -0,0 +1,88 @@
+import { TAttribute } from "@repo/utils/types";
+import { Badge, Button } from "@ui/index";
+import { ColumnDef } from "@ui/tanstack-table";
+import { LuChevronRight, LuTrash2 } from "react-icons/lu";
+
+export const column: ColumnDef<TAttribute>[] = [
+  {
+    id: "expander",
+    cell: ({ row }) => {
+      return (
+        <div>
+          {row.original.attributeValues?.length! > 0 && (
+            <button>
+              <LuChevronRight size={16} />
+            </button>
+          )}
+        </div>
+      );
+    },
+  },
+  {
+    accessorKey: "name",
+    header: "Attribute Name",
+    cell: ({ row }) => {
+      return (
+        <div>
+          <span className="font-medium">{row.original.name}</span>
+        </div>
+      );
+    },
+  },
+  {
+    id: "totalAttributes",
+    header: "Total Values",
+    cell: ({ row }) => {
+      return (
+        <div>
+          <span>{row.original.attributeValues?.length || 0}</span>
+        </div>
+      );
+    },
+  },
+  {
+    accessorKey: "attributeValues",
+    header: "Values",
+    cell: ({ row }) => {
+      return (
+        <div className="flex flex-wrap gap-4">
+          {row.original.attributeValues?.length! > 0 ? (
+            <div className="flex items-end gap-0">
+              {row.original.attributeValues?.slice(0, 5)?.map((el) => (
+                <Badge variant={"accent"} key={el.id}>
+                  {el.value}
+                </Badge>
+              ))}
+              <Badge className="-mr-3 border-none text-xs" variant={"outline"}>
+                ...more
+              </Badge>
+            </div>
+          ) : (
+            <span>No Values added yet</span>
+          )}
+        </div>
+      );
+    },
+  },
+  {
+    id: "actions",
+    header: () => {
+      return (
+        <div className="pr-3 text-end">
+          <span>Actions</span>
+        </div>
+      );
+    },
+
+    cell: () => {
+      return (
+        <div className="flex justify-end">
+          <Button className="flex h-fit items-center gap-2 py-1.5" variant={"destructive"}>
+            <LuTrash2 size={16} />
+            Delete
+          </Button>
+        </div>
+      );
+    },
+  },
+];
--- apps/admin/src/pages/attribute-list-page.tsx
+++ apps/admin/src/pages/attribute-list-page.tsx
@@ -1,8 +1,16 @@
+import { column } from "@/components/data-tables/attributes/column";
+import AttributeListDataTable from "@/components/data-tables/attributes/data-table";
 import { useGetAllAttributesWithValuesQuery } from "@repo/redux";
 
 const AttributeListPage = () => {
-  const {} = useGetAllAttributesWithValuesQuery();
-  return <div></div>;
+  const { data, isFetching } = useGetAllAttributesWithValuesQuery();
+  const attributes = data?.data || [];
+  return (
+    <div className="space-y-5">
+      <h1 className="text-xl font-medium">Attribute List</h1>
+      <AttributeListDataTable columns={column} data={attributes} isLoading={isFetching} />
+    </div>
+  );
 };
 
 export default AttributeListPage;
