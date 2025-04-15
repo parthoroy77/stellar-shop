edf1a6c5f85ccab9d6c58026f9dc3402b20245ac
Partho Roy <parthoroy117@gmail.com>
2025-04-15 06:25:20 +0000

    feat(seller): add dashboard image on landing and update design

--- apps/seller/src/components/landing/hero.tsx
+++ apps/seller/src/components/landing/hero.tsx
@@ -1,40 +1,39 @@
 import { Button, Container } from "@repo/ui";
+import Image from "next/image";
 import { LuArrowRight } from "react-icons/lu";
-
+import dashboardImg from "../../../public/dashboard-2.png";
 const Hero = () => {
   return (
-    <div>
-      <Container className="w-full py-10">
-        <div className="mx-auto">
-          <div className="flex flex-col items-center gap-12 lg:flex-row">
-            <div className="space-y-5 lg:w-1/2">
-              <h1 className="text-5xl font-bold tracking-tight text-black sm:text-6xl">
-                Transform Your Products into a<span className="text-secondary"> Thriving Business</span>
-              </h1>
-              <p className="text-accent-foreground leading-8">
-                Join thousands of successful sellers who have turned their passion into profit. Our platform provides
-                everything you need to start, grow, and scale your online business.
-              </p>
-              <div className="mt-10 flex items-center gap-x-6">
-                <Button className="px-10" size={"lg"}>
-                  Start Selling <LuArrowRight className="ml-2 inline-block h-5 w-5" />
-                </Button>
-                <Button variant={"ghost"}>
-                  Learn more <span aria-hidden="true">→</span>
-                </Button>
-              </div>
-            </div>
-            <div className="lg:w-1/2">
-              <img
-                src="https://images.unsplash.com/photo-1664575602276-acd073f104c1?q=80&w=2070&auto=format&fit=crop"
-                alt="E-commerce dashboard"
-                className="rounded-xl shadow-2xl"
-              />
+    <Container className="h-fit w-full py-10">
+      <div className="mx-auto">
+        <div className="flex flex-col items-center gap-12">
+          <div className="flex w-2/3 flex-col items-center justify-center gap-5 text-center">
+            <h1 className="text-5xl font-bold tracking-tight text-black sm:text-6xl">
+              Transform Your Products into a<span className="text-secondary"> Thriving Business</span>
+            </h1>
+            <p className="text-accent-foreground leading-8">
+              Join thousands of successful sellers who have turned their passion into profit. Our platform provides
+              everything you need to start, grow, and scale your online business.
+            </p>
+            <div className="flex items-center gap-x-6">
+              <Button className="px-10" size={"lg"}>
+                Start Selling <LuArrowRight className="ml-2 inline-block h-5 w-5" />
+              </Button>
+              <Button variant={"link"}>
+                Learn more <span aria-hidden="true">→</span>
+              </Button>
             </div>
           </div>
+          <div className="">
+            <Image
+              src={dashboardImg}
+              alt="Stellar Shop Seller Dashboard"
+              className="overflow-hidden rounded-2xl shadow-2xl"
+            />
+          </div>
         </div>
-      </Container>
-    </div>
+      </div>
+    </Container>
   );
 };
 
--- apps/seller/src/components/landing/navbar.tsx
+++ apps/seller/src/components/landing/navbar.tsx
@@ -1,15 +1,16 @@
 import { getServerAuth } from "@/lib/auth-utils";
 import { Button, Container } from "@repo/ui";
 import Link from "next/link";
+import LogoText from "../sidebar/logo-text";
 
 const Navbar = async () => {
   const { isAuthenticated } = await getServerAuth();
   return (
     <div className="py-5">
       <Container>
-        <div className="flex h-14 items-center justify-between rounded-xl border px-5">
-          <div className="text-sm font-medium md:text-base lg:text-xl">
-            <span className="text-primary">Stellar Shop</span> | <span className="text-secondary">Seller Center</span>
+        <div className="flex h-14 items-center justify-between rounded-xl">
+          <div className="flex items-center gap-2 text-sm font-medium md:text-base lg:text-xl">
+            <LogoText /> | <span className="text-secondary">Seller Center</span>
           </div>
           {isAuthenticated ? (
             <Link href={"/dashboard"}>
