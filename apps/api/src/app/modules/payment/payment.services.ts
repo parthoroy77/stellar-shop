0534e51404a995a584ebd95d0667538967732c0c
Partho Roy <parthoroy117@gmail.com>
2025-04-24 06:46:23 +0000

    feat(api): add payment initiate service and write logic for stripe payment

--- apps/api/src/app/modules/payment/payment.services.ts
+++ apps/api/src/app/modules/payment/payment.services.ts
@@ -1,3 +1,52 @@
-import { TCreateStripeCheckoutPayload } from "./payment.types";
+import prisma from "@repo/prisma/client";
+import { StatusCodes } from "http-status-codes";
+import { ApiError } from "../../handlers/ApiError";
+import { handleStripePayment } from "./payment.utils";
 
-const createStripeCheckoutSession = async ({}: TCreateStripeCheckoutPayload) => {};
+const initiate = async (orderId: string) => {
+  const order = await prisma.order.findFirst({
+    where: { uniqueId: orderId },
+    include: {
+      user: {
+        select: {
+          fullName: true,
+          email: true,
+        },
+      },
+      paymentMethod: {
+        select: {
+          name: true,
+          type: true,
+        },
+      },
+    },
+  });
+
+  if (!order) {
+    throw new ApiError(StatusCodes.NOT_FOUND, "Order not found");
+  }
+
+  if (order.status !== "PLACED") {
+    throw new ApiError(StatusCodes.NOT_FOUND, "Order is not available for payment");
+  }
+
+  if (order.paymentExpiresAt && new Date() > new Date(order.paymentExpiresAt)) {
+    throw new ApiError(StatusCodes.BAD_REQUEST, "Payment time expired. Order will be canceled.");
+  }
+
+  if (order.paymentMethod.type === "COD") {
+    throw new ApiError(StatusCodes.NOT_ACCEPTABLE, "Order for this payment set to Cash on delivery");
+  }
+
+  const providerName = order.paymentMethod.name.toLowerCase();
+
+  // Handle payment from different provider
+  switch (providerName.toLowerCase()) {
+    case "stripe":
+      return await handleStripePayment(order.id);
+    default:
+      throw new ApiError(StatusCodes.BAD_REQUEST, "Unsupported payment provider");
+  }
+};
+
+export const PaymentServices = { initiate };
---
+++ apps/api/src/app/modules/payment/payment.utils.ts
@@ -0,0 +1,56 @@
+import prisma from "@repo/prisma/client";
+import { IOrderItem } from "@repo/utils/types";
+import { StatusCodes } from "http-status-codes";
+import Stripe from "stripe";
+import config from "../../config";
+import { ApiError } from "../../handlers/ApiError";
+import { stripeInstance } from "../../services/stripe";
+
+export const handleStripePayment = async (orderId: number) => {
+  const payment = await prisma.payment.findFirst({ where: { orderId } });
+
+  if (payment?.status !== "PROCESSING") {
+    throw new ApiError(StatusCodes.NOT_FOUND, "Payment information not found");
+  }
+
+  if (payment.expiresAt && new Date() > new Date(payment.expiresAt)) {
+    throw new ApiError(StatusCodes.BAD_REQUEST, "Payment time expired. Order will be canceled.");
+  }
+
+  const orderItems = await prisma.orderItem.findMany({
+    where: { orderId },
+    select: {
+      id: true,
+      price: true,
+      quantity: true,
+      totalAmount: true,
+      product: {
+        select: { productName: true, images: { take: 1, select: { file: { select: { fileSecureUrl: true } } } } },
+      },
+    },
+  });
+
+  const lineItems: Stripe.Checkout.SessionCreateParams.LineItem[] = orderItems.map((item) => ({
+    price_data: {
+      currency: "bdt",
+      product_data: {
+        name: item.product.productName,
+        images: [item.product.images[0]?.file.fileSecureUrl] as string[],
+      },
+      unit_amount: (item as unknown as Pick<IOrderItem, "totalAmount">).totalAmount * 100,
+    },
+    quantity: item.quantity,
+  }));
+
+  const checkoutSession = await stripeInstance.checkout.sessions.create({
+    mode: "payment",
+    line_items: lineItems,
+    metadata: { orderId, paymentId: payment.id },
+    success_url: config.buyer_origin_url + `/?provider=stripe&paymentId=${payment.id}&session_id={CHECKOUT_SESSION_ID}`,
+    cancel_url: config.buyer_origin_url + "/payment-cancel",
+  });
+
+  await prisma.payment.update({ data: { status: "PENDING" }, where: { id: payment.id } });
+
+  return { redirectUrl: checkoutSession.url };
+};
